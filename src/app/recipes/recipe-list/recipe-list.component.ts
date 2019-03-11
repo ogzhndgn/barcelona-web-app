66ca0ef1a57550bd477dcf619fc10e71f50ee05a
Oguzhan Dogan <dogan_oguzhan@hotmail.com>
2019-03-11 19:52:17 +0300

    Adding ingredients & switching recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -12,7 +12,12 @@ export class RecipeListComponent {
     new Recipe('Vivamus vulputate', 'Cras luctus, lectus a faucibus bibendum, eros velit.', 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3Dbt5Cny7R&w=450&c=sc&poi=face&q=85')
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() {
   }
 
+  selectedRecipe(recipeElement: Recipe) {
+    this.recipeWasSelected.emit(recipeElement);
+  }
 }
